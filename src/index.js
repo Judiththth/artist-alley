2383b2a304dcc96864c8e9a6e3601c5d81ecd5c8
Judith Ramírez <judithramirezpachon@gmail.com>
2026-03-20 11:03:35 +0000

    estructura base creada

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Punto de entrada principal de la aplicación
+console.log('¡Bienvenido a Artist Alley!');
+
+// Aquí puedes importar tus componentes y hooks
+// import Component from './components/Component';
+// import { useCustomHook } from './hooks/useCustomHook';
