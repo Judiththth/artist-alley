cdbaa63e000afc8f3df1ca9e35c071c3836bab07
Judith Ramírez <judithramirezpachon@gmail.com>
2026-03-27 15:53:19 +0000

    eliminado src

--- src/index.js
+++
@@ -1,6 +0,0 @@
-// Punto de entrada principal de la aplicación
-console.log('¡Bienvenido a Artist Alley!');
-
-// Aquí puedes importar tus componentes y hooks
-// import Component from './components/Component';
-// import { useCustomHook } from './hooks/useCustomHook';
