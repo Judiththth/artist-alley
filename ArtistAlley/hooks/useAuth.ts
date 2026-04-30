3e659ff47e5886b987c37a1f759bf9a3d297748e
Judith Ramírez <judithramirezpachon@gmail.com>
2026-04-30 15:33:16 +0000

    creado contesxto y proveedor de autenticación. falta terminar función proveedor

--- ArtistAlley/hooks/useAuth.ts
+++ ArtistAlley/hooks/useAuth.ts
@@ -4,12 +4,18 @@ import { useState, useEffect, useContext, createContext } from 'react';
 import { User } from '../types';
 
 // Tipos para el usuario y sesión
-
-const AuthContext = createContext<AuthContextType | undefined>(undefined);
+// Primero se definen los tipos
 interface AuthContextType {
   user: User | null;
   isLoading: boolean;
 }
 
+  //Luego se crea el contexto de autenticación, que tiene 2 partes: el contexto en sí y el proveedor del contexto.
+  // Contexto
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
+// Proveedor 
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+
+}
 
