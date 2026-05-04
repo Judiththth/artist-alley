11755f38a728b9e1bf620eef8de20871560acccb
Judith Ramírez <judithramirezpachon@gmail.com>
2026-05-04 14:37:17 +0000

    useEffect agregado al proveedor de useAuth para que le pregunte a supabase si hay una sesión activa.

--- ArtistAlley/hooks/useAuth.ts
+++ ArtistAlley/hooks/useAuth.ts
@@ -2,6 +2,7 @@
 
 import { useState, useEffect, useContext, createContext } from 'react';
 import { User } from '../types';
+import supabase from '../lib/supabase';
 
 // Tipos para el usuario y sesión
 // Primero se definen los tipos
@@ -10,12 +11,31 @@ interface AuthContextType {
   isLoading: boolean;
 }
 
-  //Luego se crea el contexto de autenticación, que tiene 2 partes: el contexto en sí y el proveedor del contexto.
-  // Contexto
+//Luego se crea el contexto de autenticación, que tiene 2 partes: el contexto en sí y el proveedor del contexto.
+// Contexto
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 // Proveedor 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const { data: { session } } = await supabase.auth.getSession() // le pregunta a Supabase si hay una sesión activa
+      if (session?.user) {
+        setUser({
+          id: session.user.id,
+          email: session.user.email ?? '', // Si el email es null, se asigna una cadena vacía
+        })
+      } else {
+        setUser(null)
+      }
+    }
+    checkSession()
+  }, []);
+
+  return ();
 
 }
 
